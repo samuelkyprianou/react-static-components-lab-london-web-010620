d60f0e98c8e1c9eea1b8e1815403c2acd2a36482
samuelkyprianou <samuelkyprianou@gmail.com>
2020-03-09 10:26:31 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import CatComponent from "./CatComponent";
+import MouseComponent from "./MouseComponent";
+import GraceHopperQuoteComponent from "./GraceHopperQuoteComponent";
 
 class App extends Component {
-	render() {
-		// your code in the return statement below!
-		return (
-			<div className="App">
-				<CatComponent />
-				<GraceHopperQuoteComponent />
-				{/* one more component missing */}
-			</div>
-		);
-	}
+  render() {
+    // your code in the return statement below!
+    return (
+      <div className="App">
+        <CatComponent />
+        <GraceHopperQuoteComponent />
+        <MouseComponent />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/MouseComponent.js
+++ src/MouseComponent.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class MouseComponent extends Component {
-
   componentDidMount() {
-    require("./hoverEffect.js")
+    require("./hoverEffect.js");
   }
-  
+
   render() {
     return (
       <div className="bar" id="mouse">
         <canvas id="canvas"></canvas>
       </div>
-    )
+    );
   }
 }
+
